e7112cbecd60223e552c8406c27d9ef126ad68a6
Shirong Foo <dee.sr.foo@gmail.com>
2017-04-26 14:14:30 +0800

    post lunch changes

--- robot.rb
+++ robot.rb
@@ -1,7 +1,25 @@
 class Robot
   attr_accessor :name
 
-  def initialize(name)
-    @name = name
+  @@all_robots = []
+
+  def initialize
+    loop do
+      until check_collission
+        @name = reset
+        @@all_robots << @name
+      end
+      break
   end
+
+  def check_collision
+    @@all_robots.include? @name
+  end
+
+  def random_str
+    alphabet = ('A'..'Z').to_a
+    random_index = rand(alphabet.count)
+    alphabet[random_index]
+  end
+
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -3,41 +3,47 @@
 
 # When robots come off the factory floor, they have no name.
 
-# The first time you boot them up, a random name is generated in the format of two uppercase letters followed by three digits, such as RX837 or BC811.
+# The first time you boot them up, a random name is generated in the format of two uppercase letters followed by three digits,
+# such as RX837 or BC811.
 
-# Every once in a while we need to reset a robot to its factory settings, which means that their name gets wiped. The next time you ask, it will respond with a new random name.
+# Every once in a while we need to reset a robot to its factory settings,
+# which means that their name gets wiped. The next time you ask, it will respond with a new random name.
 
-# The names must be random: they should not follow a predictable sequence. Random names means a risk of collisions. Your solution should not allow the use of the same name twice when avoidable. In some exercism language tracks there are tests to ensure that the same name is never used twice.
-#
+# The names must be random: they should not follow a predictable sequence.
+# Random names means a risk of collisions.
+# Your solution should not allow the use of the same name twice when avoidable.
+# In some exercism language tracks there are tests to ensure that the same name is never used twice.
+
 
 require_relative '../robot'
 
 describe Robot do
   before(:context) do
-    @r1 = Robot.new('AB')
-    @r2 = Robot.new('CD')
+    @r1 = Robot.new
+    @r2 = Robot.new
   end
 
   #1 update this test, so you get a random name
   describe 'Initializatioin' do
     it 'should create new robot name' do
-      expect(@r1).not_to eq(@r2)
+      expect(@r1).to eq (/^[A-Z]{2}\d{3}$/)
+      expect(@r2).to eq (/^[A-Z]{2}\d{3}$/)
+    end
+
+    it 'should not create name that has been created' do 
+      expect(@r1.name).not_to eq(@r2.name)
     end
   end
 
   #2 if run r1.reset, name for r1 should change
   describe 'Reset' do
     it 'name for r1 should change' do
-      expect(@r1.name).not_to eq('AB')
+      original_name = @r1.name
+      @r1.reset
+      expect(@r1).not_to eq original_name
+    end
+  end
 
-    end
-  end
-  describe 'Reset' do
-    it 'should reset the name' do
-      before (:name) do
-      r2 = Robot.new
-      expect(r2.name).not_to eq()
-    end
-  end
-end
+  #3 names must be random
+
 end
