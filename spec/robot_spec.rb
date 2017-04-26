662627ce01c3c2e2e22eb4a548301bf9af6db74e
Shirong Foo <dee.sr.foo@gmail.com>
2017-04-26 10:27:48 +0800

    initialization test

---
+++ spec/robot_spec.rb
@@ -0,0 +1,23 @@
+
+# Manage robot factory settings.
+
+# When robots come off the factory floor, they have no name.
+
+# The first time you boot them up, a random name is generated in the format of two uppercase letters followed by three digits, such as RX837 or BC811.
+
+# Every once in a while we need to reset a robot to its factory settings, which means that their name gets wiped. The next time you ask, it will respond with a new random name.
+
+# The names must be random: they should not follow a predictable sequence. Random names means a risk of collisions. Your solution should not allow the use of the same name twice when avoidable. In some exercism language tracks there are tests to ensure that the same name is never used twice.
+#
+
+require_relative '../robot'
+
+describe Robot do
+  before(:context) do
+    @r1 = Robot.new()
+  end
+
+  describe 'Initializatioin' do
+    it 'should create new robot name' do
+      expect(@r1.name.length).to exist
+end
