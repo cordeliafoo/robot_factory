a4fea0521457b5a13d01da24b7184e0031399592
Shirong Foo <dee.sr.foo@gmail.com>
2017-04-26 11:55:04 +0800

    resolved more conflict

--- robot.rb
+++ robot.rb
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 class robot
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
-
-=======
-class Robot
-  attr_reader :name
-  def initialize(name = 'AB123')
-    @name = name
-  end
->>>>>>> be5f9e7925c623a0cf4dd29183dc4c9c27edbba1
 end
